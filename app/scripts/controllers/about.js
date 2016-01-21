2e3184029b1542a43e7bb0408a3fcad32c6e589d
David Villa <david.villa@heig-vd.ch>
2016-01-21 17:26:46 +0100

    Generator : yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name statsGhApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the statsGhApp
+ */
+angular.module('statsGhApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name statsGhApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the statsGhApp
+ */
+angular.module('statsGhApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
